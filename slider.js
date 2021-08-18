6c5467084b89f65025fe1806ae0f9f6635cbdefd
kazakovartem <kazakov.artem16@gmail.com>
2021-08-18 15:56:05 +0600

    Update slider.js
    no transition

--- slider.js
+++ slider.js
@@ -9,6 +9,7 @@ window.onload = function(){
 	var width = 720;
 	var op = 1;
 	var top = 0;
+	var pop = 0;
 	let disabledClick = false;
 
 	document.querySelector('.slider-arrow-right').addEventListener('click', function() {
@@ -21,21 +22,26 @@ window.onload = function(){
 		let firstDot = document.querySelector('.dots'+op);
 		firstDot.style['background-color'] = 'rgb(203, 203, 233)';
 		firstDot.style['cursor'] = 'pointer';
-		
 
-		let animation = slideContainer.animate([
-			{transform: 'translateX(' + - (top ) + 'px)'},
-			{transform: 'translateX(' + - (top + width ) + 'px)'},
-		],{
-			duration: 700,
-		});
-		animation.addEventListener('finish', function() {
-			slideContainer.style.transform = 'translateX(' + - (top ) + 'px)';
-		});
+		let sop = top;
+		let start = Date.now(); 
+		let timer = setInterval(function() {
+			let timePassed = Date.now() - start;
+			Promise.resolve().then(()=>{draw(timePassed)});
+			if (timePassed >= width) {
+				clearInterval(timer);
+				return;
+			}
+		}, 1);
+
+		function draw(timePassed) {
+			slideContainer.style['margin-left'] = -(sop +timePassed) + 'px';
+		}
+
 		top += width;
 		op ++;
 		if(op == 6){
-			slideContainer.style.transform = 'translateX(0)';
+			slideContainer.style['margin-left'] = '0 px';
 			op = 1;
 			top = 0;
 		}
@@ -46,7 +52,7 @@ window.onload = function(){
 
 		setTimeout(function () {
 			disabledClick = false;
-		}, 1000);
+		}, 800);
 	});
 
 
@@ -61,18 +67,26 @@ window.onload = function(){
 		firstDot.style['cursor'] = 'pointer';
 		op --;
 		if(op <= 0){
-			slideContainer.style.transform = 'translateX(3600px )';
+			slideContainer.style['margin-left'] = '-3600 px';
 			op = 5;
 			top = width*5;
 		}
-		
-		let animation = slideContainer.animate([
-			{transform: 'translateX(' +  - (top ) + 'px)'},
-			{transform: 'translateX(' +  - (top - width ) + 'px)'},
-		], 700);
-		animation.addEventListener('finish', function() {
-			slideContainer.style.transform = 'translateX(' + -(top ) + 'px)';
-		});
+
+		let sop = top;
+		let start = Date.now(); 
+		let timer = setInterval(function() {
+			let timePassed = Date.now() - start;
+			Promise.resolve().then(()=>{draw(timePassed)});
+			if (timePassed >= width) {
+				clearInterval(timer);
+				return;
+			}
+		}, 1);
+
+		function draw(timePassed) {
+			slideContainer.style['margin-left'] = -(sop -timePassed) + 'px';
+		}
+
 		top -= width;
 		let secondDot = document.querySelector('.dots'+op);
 		secondDot.style['background-color'] = 'rgb(154, 154, 165)';
@@ -80,7 +94,7 @@ window.onload = function(){
 
 		setTimeout(function () {
 			disabledClick = false;
-		}, 1000);
+		}, 800);
 	});
 
 	document.querySelector('.dots1').addEventListener('click', function() {
@@ -95,24 +109,33 @@ window.onload = function(){
 			firstDot.style['cursor'] = 'pointer';
 
 			top = width*(op-1);
-			console.log(top);
-			let animation = slideContainer.animate([
-				{transform: 'translateX(' +  - (top ) + 'px)'},
-				{transform: 'translateX(' +  - (top - width*(op-1) ) + 'px)'},
-			], 700);
-			animation.addEventListener('finish', function() {
-				slideContainer.style.transform = 'translateX(' + - (0) + 'px)';
-			});
+
+			let sop = top;
+			let mer = width*(op-1);
+			let start = Date.now(); 
+			let timer = setInterval(function() {
+				let timePassed = Date.now() - start;
+				Promise.resolve().then(()=>{draw(timePassed)});
+				if (timePassed >= mer) {
+					clearInterval(timer);
+					return;
+				}
+			}, 1);
+	
+			function draw(timePassed) {
+				slideContainer.style['margin-left'] = -(sop - timePassed) + 'px';
+			}
+			
+			setTimeout(function () {
+				disabledClick = false;
+			}, mer );
+
 			top = 0;
 			op = 1;
 
 			let secondDot = document.querySelector('.dots'+op);
 			secondDot.style['background-color'] = 'rgb(154, 154, 165)';
 			secondDot.style['cursor'] = 'auto';
-
-			setTimeout(function () {
-				disabledClick = false;
-			}, 1000);
 		}
 	})
 	document.querySelector('.dots2').addEventListener('click', function() {
@@ -125,36 +148,50 @@ window.onload = function(){
 			let firstDot = document.querySelector('.dots'+op);
 			firstDot.style['background-color'] = 'rgb(203, 203, 233)';
 			firstDot.style['cursor'] = 'pointer';
-
+			top = width*(op-1);
+			let sop = top;
+			let mer;
 			if(op > 2){
-				top = width*(op-1);
-				let animation = slideContainer.animate([
-					{transform: 'translateX(' +  - (top ) + 'px)'},
-					{transform: 'translateX(' +  - (top + width*(2 - op) ) + 'px)'},
-				], 700);
-				animation.addEventListener('finish', function() {
-					slideContainer.style.transform = 'translateX(' + -(width) + 'px)';
-				});
+				mer = width*(op-2);
+				let start = Date.now(); 
+				let timer = setInterval(function() {
+					let timePassed = Date.now() - start;
+					Promise.resolve().then(()=>{draw(timePassed)});
+					if (timePassed >= mer) {
+						clearInterval(timer);
+						return;
+					}
+				}, 1);
+		
+				function draw(timePassed) {
+					slideContainer.style['margin-left'] = -(sop - timePassed) + 'px';
+				}
 			}else{
-				top = width*(op-1);
-				let animation = slideContainer.animate([
-					{transform: 'translateX(' +  - (top ) + 'px)'},
-					{transform: 'translateX(' +  - (top - width*(op-2) ) + 'px)'},
-				], 700);
-				animation.addEventListener('finish', function() {
-					slideContainer.style.transform = 'translateX(' + - (width) + 'px)';
-				});
+				mer = width;
+				let start = Date.now(); 
+				let timer = setInterval(function() {
+					let timePassed = Date.now() - start;
+					Promise.resolve().then(()=>{draw(timePassed)});
+					if (timePassed >= mer) {
+						clearInterval(timer);
+						return;
+					}
+				}, 1);
+		
+				function draw(timePassed) {
+					slideContainer.style['margin-left'] = -(sop + timePassed) + 'px';
+				}
 			}
+
+			setTimeout(function () {
+				disabledClick = false;
+			}, mer);
 
 			top = width;
 			op = 2;
 			let secondDot = document.querySelector('.dots'+op);
 			secondDot.style['background-color'] = 'rgb(154, 154, 165)';
 			secondDot.style['cursor'] = 'auto';
-
-			setTimeout(function () {
-				disabledClick = false;
-			}, 1000);
 		}
 	})
 	document.querySelector('.dots3').addEventListener('click', function() {
@@ -167,36 +204,50 @@ window.onload = function(){
 			let firstDot = document.querySelector('.dots'+op);
 			firstDot.style['background-color'] = 'rgb(203, 203, 233)';
 			firstDot.style['cursor'] = 'pointer';
-
+			top = width*(op-1);
+			let sop = top;
+			let mer;
 			if(op > 3){
-				top = width*(op-1);
-				let animation = slideContainer.animate([
-					{transform: 'translateX(' +  - (top ) + 'px)'},
-					{transform: 'translateX(' +  - (top + width*(3 - op) ) + 'px)'},
-				], 700);
-				animation.addEventListener('finish', function() {
-					slideContainer.style.transform = 'translateX(' + -(width*2) + 'px)';
-				});
+				mer = width*(op-3);
+				let start = Date.now(); 
+				let timer = setInterval(function() {
+					let timePassed = Date.now() - start;
+					Promise.resolve().then(()=>{draw(timePassed)});
+					if (timePassed >= mer) {
+						clearInterval(timer);
+						return;
+					}
+				}, 1);
+		
+				function draw(timePassed) {
+					slideContainer.style['margin-left'] = -(sop - timePassed) + 'px';
+				}
 			}else{
-				top = width*(op-1);
-				let animation = slideContainer.animate([
-					{transform: 'translateX(' +  - (top ) + 'px)'},
-					{transform: 'translateX(' +  - (top - width*(op-3) ) + 'px)'},
-				], 700);
-				animation.addEventListener('finish', function() {
-					slideContainer.style.transform = 'translateX(' + - (width*2) + 'px)';
-				});
+				mer = width*(3-op);
+				let start = Date.now(); 
+				let timer = setInterval(function() {
+					let timePassed = Date.now() - start;
+					Promise.resolve().then(()=>{draw(timePassed)});
+					if (timePassed >= mer) {
+						clearInterval(timer);
+						return;
+					}
+				}, 1);
+		
+				function draw(timePassed) {
+					slideContainer.style['margin-left'] = -(sop + timePassed) + 'px';
+				}
 			}
+
+			setTimeout(function () {
+				disabledClick = false;
+			}, mer );
 
 			top = width*2;
 			op = 3;
 			let secondDot = document.querySelector('.dots'+op);
 			secondDot.style['background-color'] = 'rgb(154, 154, 165)';
 			secondDot.style['cursor'] = 'auto';
-
-			setTimeout(function () {
-				disabledClick = false;
-			}, 1000);
 		}
 	})
 	document.querySelector('.dots4').addEventListener('click', function() {
@@ -209,35 +260,51 @@ window.onload = function(){
 			let firstDot = document.querySelector('.dots'+op);
 			firstDot.style['background-color'] = 'rgb(203, 203, 233)';
 			firstDot.style['cursor'] = 'pointer';
+			let mer;
+			top = width*(op-1);
+			let sop = top;
 			if(op > 4){
-				top = width*(op-1);
-				let animation = slideContainer.animate([
-					{transform: 'translateX(' +  - (top ) + 'px)'},
-					{transform: 'translateX(' +  - (top + width*(4 - op) ) + 'px)'},
-				], 700);
-				animation.addEventListener('finish', function() {
-					slideContainer.style.transform = 'translateX(' + -(width*3) + 'px)';
-				});
+				mer = width*(op-4);
+				let start = Date.now(); 
+				let timer = setInterval(function() {
+					let timePassed = Date.now() - start;
+					Promise.resolve().then(()=>{draw(timePassed)});
+					if (timePassed >= mer) {
+						clearInterval(timer);
+						return;
+					}
+				}, 1);
+		
+				function draw(timePassed) {
+					slideContainer.style['margin-left'] = -(sop - timePassed) + 'px';
+				}
 			}else{
-				top = width*(op-1);
-				let animation = slideContainer.animate([
-					{transform: 'translateX(' +  - (top ) + 'px)'},
-					{transform: 'translateX(' +  - (top - width*(op-4) ) + 'px)'},
-				], 700);
-				animation.addEventListener('finish', function() {
-					slideContainer.style.transform = 'translateX(' + - (width*3) + 'px)';
-				});
+				
+				mer = width*(4-op);
+				let start = Date.now(); 
+				let timer = setInterval(function() {
+					let timePassed = Date.now() - start;
+					Promise.resolve().then(()=>{draw(timePassed)});
+					if (timePassed >= mer) {
+						clearInterval(timer);
+						return;
+					}
+				}, 1);
+		
+				function draw(timePassed) {
+					slideContainer.style['margin-left'] = -(sop + timePassed) + 'px';
+				}
 			}
-			
+
+			setTimeout(function () {
+				disabledClick = false;
+			},    mer );
+
 			top = width*3;
 			op = 4;
 			let secondDot = document.querySelector('.dots'+op);
 			secondDot.style['background-color'] = 'rgb(154, 154, 165)';
 			secondDot.style['cursor'] = 'auto';
-
-			setTimeout(function () {
-				disabledClick = false;
-			}, 1000);
 		}
 	})
 	document.querySelector('.dots5').addEventListener('click', function() {
@@ -252,23 +319,35 @@ window.onload = function(){
 			firstDot.style['cursor'] = 'pointer';
 
 			top = width*(op-1);
-			let animation = slideContainer.animate([
-				{transform: 'translateX(' +  - (top ) + 'px)'},
-				{transform: 'translateX(' +  - (top + width*(5 - op) ) + 'px)'},
-			], 700);
-			animation.addEventListener('finish', function() {
-				slideContainer.style.transform = 'translateX(' + -(width*4) + 'px)';
-			});
 			
+			
+			let sop = top;
+			let mer = width*(5-op);
+			let start = Date.now(); 
+			let timer = setInterval(function() {
+				let timePassed = Date.now() - start;
+				Promise.resolve().then(()=>{draw(timePassed)});
+				if (timePassed >= mer) {
+					clearInterval(timer);
+					return;
+				}
+			}, 1);
+	
+			function draw(timePassed) {
+				slideContainer.style['margin-left'] = -(sop + timePassed) + 'px';
+			}
+
+
+			setTimeout(function () {
+				disabledClick = false;
+			},  mer );
+
 			top = width*4;
 			op = 5;
 			let secondDot = document.querySelector('.dots'+op);
 			secondDot.style['background-color'] = 'rgb(154, 154, 165)';
 			secondDot.style['cursor'] = 'auto';
 
-			setTimeout(function () {
-				disabledClick = false;
-			}, 1000);
 		}
 	})
 }
